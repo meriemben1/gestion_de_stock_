64a0b4c20e5faa159418322d98828236d34498ad
meriemben1 <159068199+meriemben1@users.noreply.github.com>
2024-04-13 19:15:44 +0100

    Create fournisseurrep.java

---
+++ fournisseurrep.java
@@ -0,0 +1,9 @@
+package repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import models.fournisseur;
+
+public interface fournissuerrep extends JpaRepository<fournisseur, Long> {
+
+}
